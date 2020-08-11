076e730b85fe7c926ec67a625419d653b71aed8b
Ivan Trujillo <ivanrts43@gmail.com>
2020-08-11 14:01:49 -0400

    Set up server using express, added post route handler to add a new book, added function to check if book exists, added error return when errors occur, added delete route handler with Library check in memory, added check function and error handling

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const bp = require("body-parser");
-const axios = require("axios");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
@@ -9,18 +8,50 @@ app.use(bp.json());
 app.use(bp.urlencoded({extended: true}));
 app.use(cors());
 
-const cartUrl = "https://gopuff-public.s3.amazonaws.com/dev-assignments/product/order.json"
-let ProductState;
-app.get('/getCart', async(req, res) =>{
+const Library = [];
+app.post('/add-book-to-library', async(req, res) =>{
+    const { book } = req.body
     try {
-        return await getCart()
+        const result = await addBook(book)
+        if(result.stack && result.message || result instanceof Error){
+            return res.status(400).json({ success: false, message: result.message})
+        }
+        console.log(Library)
+        return res.status(200).json({success: true, message: "Book added successfully."});
+    }catch(e) {
+        console.error(e.stack)
+        return res.status(400).json({ message: e.message })
+    }
+    async function addBook(book) {
+        const isNewBook = await doesBookExist(book, Library)
+        if(isNewBook === false) {
+            Library.push(book);
+            return Library.find(bookTitle => bookTitle === book);
+        } else {
+            return new Error('Book already exists, please add a new book.')
+        }
+    }
+})
+app.delete('/delete-from-library', async(req, res) =>{
+    const { book } = req.body;
+    try {
+        const result = await removeBook(book);
+        if(result.stack && result.message || result instanceof Error){
+            return res.status(400).json({ success: false, message: result.message})
+        }
+        return res.status(200).json({ message: "Successfully removed book.", success: result })
     } catch (error) {
         console.error(error)
     }
-    async function getCart() {
-        const data = await axios.get(cartUrl).then(resp => resp.data.cart)
-        ProductState = data.products
-        return await res.status(200).json(data);
+    async function removeBook(book) {
+        const isBookInLibrary = await doesBookExist(book, Library)
+        if(isBookInLibrary === false) {
+            return new Error("Book does not exist, please enter a title in our database.")
+        } else {
+            const indexOfTitle = Library.indexOf(book)
+            Library.splice(indexOfTitle, 1)
+            return true
+        }
     }
 })
 app.put('/addProducts', async(req, res) =>{
@@ -29,6 +60,19 @@ app.put('/addProducts', async(req, res) =>{
 app.put('/removeProducts', async(req, res) =>{
     
 })
+
 app.listen(PORT, function () {
     console.log('listen to events on a "port: ', PORT)
 });
+
+async function doesBookExist(newBook) {
+    try {
+        console.log(newBook, "Book to add")
+        const isBookInLibrary = Library.find(book => book === newBook)
+        console.log(isBookInLibrary, "this is the existance of the boook")
+        if(isBookInLibrary == null) return false
+        return true 
+    }catch(e) {
+        console.error(e.message)
+    }
+} 
