a02a7133307cf9ad6a533c5edca66c2b8aa4bc62
Ivan Trujillo <ivanrts43@gmail.com>
2020-08-11 14:35:29 -0400

    Added patch route to update book titles

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bp.json());
 app.use(bp.urlencoded({extended: true}));
 app.use(cors());
 
-const Library = [];
+const Library = ['test1', 'test2', 'test3'];
 app.post('/add-book-to-library', async(req, res) =>{
     const { book } = req.body
     try {
@@ -54,8 +54,38 @@ app.delete('/delete-from-library', async(req, res) =>{
         }
     }
 })
-app.put('/addProducts', async(req, res) =>{
-
+app.patch('/update-book', async(req, res) =>{
+    const { original_book, new_book } = req.body
+    try{
+        const result = await updateBook(original_book, new_book)
+        if(result.stack && result.message || result instanceof Error){
+            return res.status(400).json({ success: false, message: result.message})
+        }
+        return res.status(200).json({ success: result, message: "Book added successfully. "})
+    }catch(e) {
+        console.error(e.stack)
+        return res.json({ message: e.message, success: false })
+    }
+    async function updateBook(originalBook, updatedBook) {
+        try{
+            const doesOriginalExist = await doesBookExist(originalBook);
+            if(doesOriginalExist === false){
+                return new Error("The book you want to update does not exist in Library.")
+            }
+            const isUpdatedBookInLibrary = await doesBookExist(updatedBook)
+            if(isUpdatedBookInLibrary === true) {
+                return new Error("The book title you are updating to already exists.")
+            }
+            if(doesOriginalExist === true && isUpdatedBookInLibrary === false) {
+                const indexToReplace = Library.indexOf(original_book)
+                Library.splice(indexToReplace,1,updatedBook)
+                return true
+            }
+        }catch(e) {
+            console.error(e.stack)
+            return new Error("There was a system problem.")
+        }
+    }
 })
 app.put('/removeProducts', async(req, res) =>{
     
